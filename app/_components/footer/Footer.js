28cf04e995b92ce36aefe1cdc0fe2a4340bb63a2
abhishek <abhishekpal2500@gmail.com>
2024-10-17 21:00:57 +0530

    footer unused code removed

--- app/_components/footer/Footer.js
+++ app/_components/footer/Footer.js
@@ -1,8 +1,6 @@
 "use client"
-import { Box, Grid2, List, ListItem, Typography } from "@mui/material"
-import logo from "../../_assets/pal-brothers-group.PNG"
-import Link from "next/link"
-import Image from "next/image"
+import { Grid2, Typography } from "@mui/material"
+
 import "./footer.css"
 
 export const Footer = () => {
@@ -15,40 +13,6 @@ export const Footer = () => {
                 <Grid2 size={12}>
                 <Typography variant="body2" sx={{ p: 2, textAlign: "center" }} color="initial">Copyright  &copy; 2013-{fullYear} Pal Brothers Group All rights reserved.</Typography>
                 </Grid2>
-                {/* <Grid2 size={4}>
-                    <Box className="footer-link">
-                        <List className='menu-ul'>
-                            <ListItem>
-                                <Link href="#" underline="none">
-                                    Saree
-                                </Link>
-                            </ListItem>
-                            <ListItem>
-                                <Link href="#" underline="none">
-                                    Lehanga & Chunari
-                                </Link>
-                            </ListItem>
-                        </List>
-                    </Box>
-                </Grid2>
-                <Grid2 size={4}>
-                    <Box sx={{ textAlign: "center" }}>
-                        <Link href="/"><Image
-                            src={logo}
-                            width={250}
-                            height={70}
-                            alt="pal-brothers-group" /></Link>
-
-                        <Typography variant="body1" color="initial">Call: <strong>8317068171</strong>, <strong>9892299240</strong></Typography>
-                    </Box>
-                </Grid2>
-                <Grid2 size={4}>
-                    <Typography variant="h6" color="initial">Contact and Location</Typography>
-                    <Typography variant="body1" color="initial">
-                        Pal Brothers Vastralaya, Belwa sengar, Rudhauli-Bakhira Marg,
-                        Sant Kabir Nagar, 272148
-                    </Typography>
-                </Grid2> */}
             </Grid2>
         </footer>
     )
